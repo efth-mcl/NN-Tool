cccc0b2f2303f7b746155139c0a46bf943106f0b
efth-mcl <efthymis.michalis@pm.me>
2018-01-26 02:16:58 +0200

    change code style

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 import os
+
+
 if(not(os.path.exists('CSVRESULTS'))):
     os.makedirs('CSVRESULTS')
 
@@ -13,4 +15,5 @@ if(not(os.path.exists('TOPOLOGYS'))):
 
 if(not(os.path.exists('newproject.py'))):
     with open('newproject.py', 'w') as Pyfile:
-        Pyfile.write("""import os;PrName=input('Give Project Name: ');SCfile = open('SCRIPTS/'+PrName+'_SC.py','w');SCfile.write("import sys;sys.path.append('../LIBRARY');from NEURAL_NETWORK_TOOL import *");SCfile.close();os.makedirs('TOPOLOGYS/'+PrName+'_TP');os.system('touch TOPOLOGYS/'+PrName+'_TP/topology.txt');os.makedirs('CSVRESULTS/'+PrName+'_RS')""")
+        Commants = """import os;PrName=input('Give Project Name: ');SCfile = open('SCRIPTS/'+PrName+'_SC.py','w');SCfile.write("import sys;sys.path.append('../LIBRARY');from NEURAL_NETWORK_TOOL import *");SCfile.close();os.makedirs('TOPOLOGYS/'+PrName+'_TP');os.system('touch TOPOLOGYS/'+PrName+'_TP/topology.txt');os.makedirs('CSVRESULTS/'+PrName+'_RS')"""
+        Pyfile.write(Commants)
